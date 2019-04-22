8bb4ffad32b7c3f1dd532103f9c23ae430027bc1
Sergey <12kaptan12@gmail.com>
2019-04-22 15:09:30 +0300

    Initial Commit

---
+++ Algorithms/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Algorithms
+//
+//  Created by Sergey Koriukin on 22/04/2019.
+//  Copyright © 2019 Sergey Koriukin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
